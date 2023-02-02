8ce8e3d201c5e28e2465798483e05674c5dda337
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-01 20:41:43 -0800

    add state to toggle on menu links and hover color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
-import {FaFacebook,FaTwitter} from "react-icons/fa";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="relative flex justify-between items-center px-8 py-12">
       <div>
@@ -16,40 +18,60 @@ const Header = () => {
           src="./images/icon-hamburger.svg"
           alt="icon-hamburger"
           className="cursor-pointer"
+          onClick={() => setIsOpen(true)}
         />
       </div>
-      <div className="absolute top-0 bottom-0 left-0 right-0 h-screen bg-mobileBg text-2xl w-full py-12 px-8">
-        <div className="flex justify-between items-center">
-          <img
-            src="./images/logo-bookmark.svg"
-            alt="logo-bookmark"
-            className="img-filter w-40 cursor-pointer"
-          />
-          <AiOutlineClose className="text-gray-300 cursor-pointer" size={30} />
+      {isOpen && (
+        <div className="absolute top-0 bottom-0 left-0 right-0 h-screen bg-mobileBg text-2xl w-full py-12 px-8">
+          <div className="flex justify-between items-center">
+            <img
+              src="./images/logo-bookmark.svg"
+              alt="logo-bookmark"
+              className="img-filter w-40 cursor-pointer"
+            />
+            <div
+              className="text-gray-300 cursor-pointer hover:text-SoftRed transition-all duration-200"
+              onClick={() => setIsOpen(false)}
+            >
+              <AiOutlineClose size={30} />
+            </div>
+          </div>
+          <nav className="mt-12 flex flex-col items-center">
+            <a
+              href="#"
+              className="py-6 border-t border-t-mobileBorder w-full text-gray-300 text-center uppercase hover:text-SoftRed transition-all duration-200"
+            >
+              features
+            </a>
+            <a
+              href="#"
+              className="py-6 border-t border-t-mobileBorder w-full text-gray-300 text-center uppercase hover:text-SoftRed transition-all duration-200"
+            >
+              pricing
+            </a>
+            <a
+              href="#"
+              className="py-6 border-t border-b border-y-mobileBorder w-full text-gray-300 text-center uppercase mb-8 hover:text-SoftRed transition-all duration-200"
+            >
+              contact
+            </a>
+            <a
+              href="#"
+              className="py-4 border border-white w-full rounded-md text-gray-300 text-center uppercase hover:text-SoftRed hover:border-SoftRed transition-all duration-200"
+            >
+              login
+            </a>
+          </nav>
+          <div className="mt-32 flex justify-center items-center">
+            <a href="#" className="text-gray-300 mr-12 hover:text-SoftRed transition-all duration-200">
+              <FaFacebook size={40} />
+            </a>
+            <a href="#" className="text-gray-300 hover:text-SoftRed transition-all duration-200">
+              <FaTwitter size={40} />
+            </a>
+          </div>
         </div>
-        <nav className="mt-12 flex flex-col items-center">
-          <a href="#" className="py-6 border-t border-t-mobileBorder w-full text-gray-300 text-center uppercase">
-            features
-          </a>
-          <a href="#" className="py-6 border-t border-t-mobileBorder w-full text-gray-300 text-center uppercase">
-            pricing
-          </a>
-          <a href="#" className="py-6 border-t border-b border-y-mobileBorder w-full text-gray-300 text-center uppercase mb-8">
-            contact
-          </a>
-          <a href="#" className="py-4 border border-white w-full rounded-md text-gray-300 text-center uppercase">
-            login
-          </a>
-        </nav>
-        <div className="mt-40 flex justify-center items-center">
-        <a href="#" className="text-gray-300 mr-12">
-        <FaFacebook size={40}/>
-        </a>
-        <a href="#" className="text-gray-300">
-          <FaTwitter size={40}/>
-        </a>
-        </div>
-      </div>
+      )}
     </header>
   );
 };
