a6d978181b15a1b979fbffad179e82feffbcf927
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-01 20:32:46 -0800

    style the navbar for mobile srceen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,57 @@
-import {AiOutlineClose} from "react-icons/ai"
+import { AiOutlineClose } from "react-icons/ai";
+import {FaFacebook,FaTwitter} from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className='relative flex justify-between items-center px-6 py-12'>
+    <header className="relative flex justify-between items-center px-8 py-12">
       <div>
-        <img src="./images/logo-bookmark.svg" alt="logo-bookmark" className="cursor-pointer"/>
+        <img
+          src="./images/logo-bookmark.svg"
+          alt="logo-bookmark"
+          className="cursor-pointer"
+        />
       </div>
       <div>
-        <img src="./images/icon-hamburger.svg" alt="icon-hamburger" className="cursor-pointer" />
+        <img
+          src="./images/icon-hamburger.svg"
+          alt="icon-hamburger"
+          className="cursor-pointer"
+        />
       </div>
-      <div className="absolute top-0 bottom-0 left-0 right-0 h-screen bg-navMobile text-4xl w-full p-8">
-      <div className="flex justify-between items-center">
-      <img src="./images/logo-bookmark.svg" alt="logo-bookmark" className="img-filter w-40" />
-        <AiOutlineClose className="text-gray-300 cursor-pointer" size={30}/>
+      <div className="absolute top-0 bottom-0 left-0 right-0 h-screen bg-mobileBg text-2xl w-full py-12 px-8">
+        <div className="flex justify-between items-center">
+          <img
+            src="./images/logo-bookmark.svg"
+            alt="logo-bookmark"
+            className="img-filter w-40 cursor-pointer"
+          />
+          <AiOutlineClose className="text-gray-300 cursor-pointer" size={30} />
+        </div>
+        <nav className="mt-12 flex flex-col items-center">
+          <a href="#" className="py-6 border-t border-t-mobileBorder w-full text-gray-300 text-center uppercase">
+            features
+          </a>
+          <a href="#" className="py-6 border-t border-t-mobileBorder w-full text-gray-300 text-center uppercase">
+            pricing
+          </a>
+          <a href="#" className="py-6 border-t border-b border-y-mobileBorder w-full text-gray-300 text-center uppercase mb-8">
+            contact
+          </a>
+          <a href="#" className="py-4 border border-white w-full rounded-md text-gray-300 text-center uppercase">
+            login
+          </a>
+        </nav>
+        <div className="mt-40 flex justify-center items-center">
+        <a href="#" className="text-gray-300 mr-12">
+        <FaFacebook size={40}/>
+        </a>
+        <a href="#" className="text-gray-300">
+          <FaTwitter size={40}/>
+        </a>
+        </div>
       </div>
-      <nav>
-        <ul>
-          <li>features</li>
-          <li>pricing</li>
-          <li>contact</li>
-        </ul>
-        <button>login</button>
-      </nav>
-      </div>
-
     </header>
   );
-}
+};
 
 export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,8 @@ module.exports = {
         SoftRed: "hsl(0, 94%, 66%)",
         GrayishBlue: "hsl(229, 8%, 60%)",
         VeryDarkBlue: "hsl(229, 31%, 21%)",
-        navMobile : "hsla(229, 31%, 11%, 0.9)"
+        mobileBg : "hsla(229, 31%, 11%, 0.9)",
+        mobileBorder: "rgba(255,255,255,0.2)"
       },
     },
   },
