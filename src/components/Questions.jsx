76e9ba7626d15a25eb8cb69f908923a96b224751
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-04 10:55:13 -0800

    add questions data

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,58 +1,121 @@
 
 
 const Questions = () => {
+
   return (
     <section className="px-8 py-12 lg:mt-16">
       <div className="text-center max-w-sm mx-auto sm:max-w-lg lg:max-w-2xl">
-        <h1 className="text-VeryDarkBlue text-3xl font-medium sm:text-4xl lg:text-5xl">Frequently Asked Questions</h1>
-        <p className="text-GrayishBlue mt-5 mb-16 md:text-xl">Here are some of our FAQs. If you have any other questions you’d like answered please feel free to email us.</p>
+        <h1 className="text-VeryDarkBlue text-3xl font-medium sm:text-4xl lg:text-5xl">
+          Frequently Asked Questions
+        </h1>
+        <p className="text-GrayishBlue mt-5 mb-16 md:text-xl">
+          Here are some of our FAQs. If you have any other questions you’d like
+          answered please feel free to email us.
+        </p>
       </div>
       <div className="w-full m-auto sm:max-w-[600px]">
         <div className="py-8 border-b border-b-GrayishBlue">
-        <div className="flex justify-between items-center">
-          <h3 className="text-VeryDarkBlue font-medium md:text-xl">What is Bookmark?</h3>
-          <img src="./images/icon-arrow.svg" alt="icon-arrow" className="w-[25px] cursor-pointer" />
-        </div>
-        <p className="text-GrayishBlue mt-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.
-        </p>
-        </div>
-      </div>
-      <div className="w-full m-auto sm:max-w-[600px]">
-        <div className="py-8 border-b border-b-GrayishBlue">
-        <div className="flex justify-between items-center">
-          <h3 className="text-VeryDarkBlue font-medium md:text-xl">How can I request a new browser?</h3>
-          <img src="./images/icon-arrow.svg" alt="icon-arrow" className="w-[25px] cursor-pointer" />
-        </div>
-        <p className="text-GrayishBlue mt-5">
-        Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.
-        </p>
-        </div>
-      </div>
-      <div className="w-full m-auto sm:max-w-[600px]">
-        <div className="py-8 border-b border-b-GrayishBlue">
-        <div className="flex justify-between items-center">
-          <h3 className="text-VeryDarkBlue font-medium md:text-xl">Is there a mobile app?</h3>
-          <img src="./images/icon-arrow.svg" alt="icon-arrow" className="w-[25px] cursor-pointer" />
-        </div>
-        <p className="text-GrayishBlue mt-5">
-        Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.
-        </p>
+          <div className="flex justify-between items-center">
+            <h3 className="text-VeryDarkBlue font-medium md:text-xl">
+              What is Bookmark?
+            </h3>
+            <img
+              src="./images/icon-arrow.svg"
+              alt="icon-arrow"
+              className={`w-[25px] cursor-pointer ${
+                question === "q1" ? "img-active" : ""
+              }`}
+              onClick={handleClickFirst}
+            />
+          </div>
+          {question === "q1" && (
+            <p className="text-GrayishBlue mt-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
+              quis quam ornare mattis.
+            </p>
+          )}
         </div>
       </div>
       <div className="w-full m-auto sm:max-w-[600px]">
         <div className="py-8 border-b border-b-GrayishBlue">
-        <div className="flex justify-between items-center">
-          <h3 className="text-VeryDarkBlue font-medium md:text-xl">What about other Chromium browsers?</h3>
-          <img src="./images/icon-arrow.svg" alt="icon-arrow" className="w-[25px] cursor-pointer" />
+          <div className="flex justify-between items-center">
+            <h3 className="text-VeryDarkBlue font-medium md:text-xl">
+              How can I request a new browser?
+            </h3>
+            <img
+              src="./images/icon-arrow.svg"
+              alt="icon-arrow"
+              className={`w-[25px] cursor-pointer ${
+                question === "q2" ? "img-active" : ""
+              }`}
+              onClick={handleClickSecond}
+            />
+          </div>
+          {question === "q2" && (
+            <p className="text-GrayishBlue mt-5">
+              Vivamus luctus eros aliquet convallis ultricies. Mauris augue
+              massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus
+              eros aliquet convallis ultricies. Mauris augue massa, ultricies
+              non ligula. Suspendisse imperdie tVivamus luctus eros aliquet
+              convallis ultricies. Mauris augue massa, ultricies non ligula.
+              Suspendisse imperdiet.
+            </p>
+          )}
         </div>
-        <p className="text-GrayishBlue mt-5">
-        Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit. Thank you for reading it and testing my project !
-        </p>
+      </div>
+      <div className="w-full m-auto sm:max-w-[600px]">
+        <div className="py-8 border-b border-b-GrayishBlue">
+          <div className="flex justify-between items-center">
+            <h3 className="text-VeryDarkBlue font-medium md:text-xl">
+              Is there a mobile app?
+            </h3>
+            <img
+              src="./images/icon-arrow.svg"
+              alt="icon-arrow"
+              className={`w-[25px] cursor-pointer ${
+                question === "q3" ? "img-active" : ""
+              }`}
+              onClick={handleClickThird}
+            />
+          </div>
+          {question === "q3" && (
+            <p className="text-GrayishBlue mt-5">
+              Sed consectetur quam id neque fermentum accumsan. Praesent luctus
+              vestibulum dolor, ut condimentum urna vulputate eget. Cras in
+              ligula quis est pharetra mattis sit amet pharetra purus. Sed
+              sollicitudin ex et ultricies bibendum.
+            </p>
+          )}
+        </div>
+      </div>
+      <div className="w-full m-auto sm:max-w-[600px]">
+        <div className="py-8 border-b border-b-GrayishBlue">
+          <div className="flex justify-between items-center">
+            <h3 className="text-VeryDarkBlue font-medium md:text-xl">
+              What about other Chromium browsers?
+            </h3>
+            <img
+              src="./images/icon-arrow.svg"
+              alt="icon-arrow"
+              className={`w-[25px] cursor-pointer ${
+                question === "q4" ? "img-active" : ""
+              }`}
+              onClick={handleClickFourth}
+            />
+          </div>
+          {question === "q4" && (
+            <p className="text-GrayishBlue mt-5">
+              Integer condimentum ipsum id imperdiet finibus. Vivamus in
+              placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida
+              pellentesque non ut velit. Thank you for reading it and testing my
+              project !
+            </p>
+          )}
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default Questions;
