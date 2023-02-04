261a101e84019f2b15b3ceae250a53ea4a1bb93e
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-04 10:24:09 -0800

    add question card to mobile screen

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -7,6 +7,17 @@ const Questions = () => {
         <h1 className="text-VeryDarkBlue text-3xl font-medium sm:text-4xl lg:text-5xl">Frequently Asked Questions</h1>
         <p className="text-GrayishBlue mt-5 mb-16 md:text-xl">Here are some of our FAQs. If you have any other questions you’d like answered please feel free to email us.</p>
       </div>
+      <div className="w-full m-auto">
+        <div className="py-8">
+        <div className="flex justify-between items-center">
+          <h3 className="text-VeryDarkBlue font-medium">What is Bookmark?</h3>
+          <img src="./images/icon-arrow.svg" alt="icon-arrow" className="w-[25px]" />
+        </div>
+        <p className="text-GrayishBlue mt-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.
+        </p>
+        </div>
+      </div>
     </section>
   );
 }
