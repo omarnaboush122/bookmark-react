06a451784edbbc8dae95e300b90add8fdde165e6
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-03 16:48:26 -0800

    add all features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -28,7 +28,7 @@ const Features = () => {
       <button onClick={tab2} className={`relative py-4 text-GrayishBlue cursor-pointer w-72 mx-auto border-b border-b-gray-500 ${tab === "tab2" ? "active" : ""} transition-all duration-300 lg:text-xl lg:border-0`}>Speedy Searching</button>
       <button onClick={tab3} className={`relative py-4 text-GrayishBlue cursor-pointer w-72 mx-auto border-b border-b-gray-500 ${tab === "tab3" ? "active" : ""} transition-all duration-300 lg:text-xl lg:border-0`}>Easy Searching</button>
     </div>
-    <article className="mt-20 flex flex-col items-center lg:flex-row lg:w-[1000px] lg:h-[420px] lg:mx-auto">
+    <article className={`mt-20 ${tab === "tab1" ? "flex" : "hidden"} flex-col items-center lg:flex-row lg:w-[1000px] lg:h-[420px] lg:mx-auto`}>
       <div className="relative">
         <img className="w-[300px] sm:w-[500px]" src="./images/illustration-features-tab-1.svg" alt="illustration-features-tab-1" />
         <div className="absolute bg-SoftBlue h-52 w-[30rem] top-9 -left-60 -z-10 border-b rounded-br-full rounded-tr-full sm:w-[45rem] sm:h-[22rem] sm:-left-[20.5rem] md:[]"></div>
@@ -38,7 +38,33 @@ const Features = () => {
         <p className="text-base mt-4 text-GrayishBlue sm:text-lg lg:text-xl lg:mb-8">
         Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.
         </p>
-        <button className="hidden lg:block lg:text-white lg:bg-SoftBlue lg:py-4 lg:px-8 lg:rounded-md lg:border lg:hover:bg-white lg:hover:border-SoftBlue lg:hover:text-SoftBlue transition-all duration-300">More Info</button>
+        <button className="hidden lg:block lg:text-white lg:bg-SoftBlue lg:py-4 lg:px-8 lg:rounded-md lg:border lg:shadow-md lg:hover:bg-white lg:hover:border-SoftBlue lg:hover:text-SoftBlue transition-all duration-300">More Info</button>
+      </div>
+    </article>
+    <article className={`mt-20 ${tab === "tab2" ? "flex" : "hidden"} flex-col items-center lg:flex-row lg:w-[1000px] lg:h-[420px] lg:mx-auto`}>
+      <div className="relative">
+        <img className="w-[300px] sm:w-[500px]" src="./images/illustration-features-tab-2.svg" alt="illustration-features-tab-2" />
+        <div className="absolute bg-SoftBlue h-52 w-[30rem] top-9 -left-60 -z-10 border-b rounded-br-full rounded-tr-full sm:w-[45rem] sm:h-[22rem] sm:-left-[20.5rem] md:[]"></div>
+      </div>
+      <div className="max-w-[400px] mx-auto text-center mt-24 sm:mt-32 lg:mt-0 lg:ml-16 lg:text-left lg:max-w-[480px]">
+        <h1 className="text-2xl text-VeryDarkBlue font-medium sm:text-3xl lg:text-4xl">Intelligent search</h1>
+        <p className="text-base mt-4 text-GrayishBlue sm:text-lg lg:text-xl lg:mb-8">
+        Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.
+        </p>
+        <button className="hidden lg:block lg:text-white lg:bg-SoftBlue lg:py-4 lg:px-8 lg:rounded-md lg:border lg:shadow-md lg:hover:bg-white lg:hover:border-SoftBlue lg:hover:text-SoftBlue transition-all duration-300">More Info</button>
+      </div>
+    </article>
+    <article className={`mt-20 ${tab === "tab3" ? "flex" : "hidden"} flex-col items-center lg:flex-row lg:w-[1000px] lg:h-[420px] lg:mx-auto`}>
+      <div className="relative">
+        <img className="w-[300px] sm:w-[500px]" src="./images/illustration-features-tab-3.svg" alt="illustration-features-tab-3" />
+        <div className="absolute bg-SoftBlue h-52 w-[30rem] top-9 -left-60 -z-10 border-b rounded-br-full rounded-tr-full sm:w-[45rem] sm:h-[22rem] sm:-left-[20.5rem] md:[]"></div>
+      </div>
+      <div className="max-w-[400px] mx-auto text-center mt-24 sm:mt-32 lg:mt-0 lg:ml-16 lg:text-left lg:max-w-[480px]">
+        <h1 className="text-2xl text-VeryDarkBlue font-medium sm:text-3xl lg:text-4xl">Share your bookmarks</h1>
+        <p className="text-base mt-4 text-GrayishBlue sm:text-lg lg:text-xl lg:mb-8">
+        Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.
+        </p>
+        <button className="hidden lg:block lg:text-white lg:bg-SoftBlue lg:py-4 lg:px-8 lg:rounded-md lg:border lg:shadow-md lg:hover:bg-white lg:hover:border-SoftBlue lg:hover:text-SoftBlue transition-all duration-300">More Info</button>
       </div>
     </article>
     </section>
