02a73b245ab21be17b5ff6ae8095b4aa1667d5c5
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-03 16:28:16 -0800

    style tab feature for tablet screen

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -23,15 +23,15 @@ const Features = () => {
       <h1 className="text-VeryDarkBlue text-3xl font-medium md:text-4xl">Features</h1>
       <p className="text-GrayishBlue text-base max-w-[600px] my-8 mx-auto md:text-lg lg:text-xl">Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
     </div>
-    <div className="relative flex flex-col max-w-4xl mx-auto lg:flex-row lg:border lg:border-b-gray-500">
+    <div className="relative flex flex-col max-w-4xl mx-auto lg:flex-row lg:border-b lg:border-b-gray-500">
       <button onClick={tab1} className={`relative py-4 text-GrayishBlue cursor-pointer w-72 mx-auto border-t border-t-gray-500 border-b border-b-gray-500 ${tab === "tab1" ? "active" : ""} transition-all duration-300 lg:text-xl lg:border-0`}>Simple Bookmarking</button>
       <button onClick={tab2} className={`relative py-4 text-GrayishBlue cursor-pointer w-72 mx-auto border-b border-b-gray-500 ${tab === "tab2" ? "active" : ""} transition-all duration-300 lg:text-xl lg:border-0`}>Speedy Searching</button>
       <button onClick={tab3} className={`relative py-4 text-GrayishBlue cursor-pointer w-72 mx-auto border-b border-b-gray-500 ${tab === "tab3" ? "active" : ""} transition-all duration-300 lg:text-xl lg:border-0`}>Easy Searching</button>
     </div>
     <article className="mt-20 flex flex-col items-center">
       <div className="relative">
-        <img className="w-[300px]" src="./images/illustration-features-tab-1.svg" alt="illustration-features-tab-1" />
-        <div className="absolute bg-SoftBlue h-52 w-72 top-9 -left-10 -z-10 border-b rounded-br-full rounded-tr-full"></div>
+        <img className="w-[300px] sm:w-[500px]" src="./images/illustration-features-tab-1.svg" alt="illustration-features-tab-1" />
+        <div className="absolute bg-SoftBlue h-52 w-[30rem] top-9 -left-60 -z-10 border-b rounded-br-full rounded-tr-full sm:w-[45rem] sm:h-[22rem] sm:-left-[20.5rem] md:[]"></div>
       </div>
       <div className="max-w-[400px] mx-auto text-center mt-24">
         <h1 className="text-2xl text-VeryDarkBlue font-medium">Bookmark in one click</h1>
