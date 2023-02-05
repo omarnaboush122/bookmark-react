78dc1a6044953d529fc6a64e81662d2f1a7fc091
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-04 18:15:56 -0800

    add state to validate the email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,64 @@
-
+import { useState } from "react";
 
 const Contact = () => {
+  const [email, setEmail] = useState("");
+  const [isError,setIsError] = useState(false);
+  const [isCorrect,setIsCorrect] = useState(false);
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(validateEmail(email)) {
+      
+    }
+  }
+
   return (
     <section className="bg-SoftBlue px-8 py-12 text-white text-center">
-    <p className="text-sm tracking-[0.3rem] uppercase">35,000+ already joined</p>
-        <h3 className="max-w-[500px] mx-auto mt-4 mb-6 text-2xl font-medium md:text-3xl lg:text-4xl lg:mb-12">Stay up-to-date with what we’re doing</h3>
-        <form className="flex flex-col items-center md:flex-row md:items-start md:justify-center">
-          <div className="relative p-1 pb-8 mb-5 bg-SoftRed rounded-md">
-          <input type="email" placeholder="Enter your email address" className="w-[300px] sm:w-[400px] mx-auto py-3 px-6 text-VeryDarkBlue rounded-md border outline-none md:mx-0" />
-          <img src="./images/icon-error.svg" alt="error-icon" className="absolute right-5 top-5" />
-          <p className="absolute left-[15px] bottom-[10px] text-xs italic">Whoops, make sure it's an email</p>
-          </div>
-          <button type="submit" className="w-[300px] bg-SoftRed sm:w-[400px] mx-auto px-6 py-3 rounded-md border border-SoftRed shadow-md hover:bg-white hover:border-SoftRed hover:text-SoftRed transition-all duration-300 md:m-0 md:ml-6 md:w-[150px]">Contact Us</button>
-        </form>
+      <p className="text-sm tracking-[0.3rem] uppercase lg:text-base">
+        35,000+ already joined
+      </p>
+      <h3 className="max-w-[500px] mx-auto mt-4 mb-6 text-2xl sm:text-3xl md:text-4xl lg:max-w-[600px] lg:text-5xl lg:mb-12">
+        Stay up-to-date with what we’re doing
+      </h3>
+      <form className="flex flex-col items-center md:flex-row md:items-start md:justify-center">
+        <div className="relative p-1 pb-8 mb-5 bg-SoftRed rounded-md">
+          <input
+            type="email"
+            placeholder="Enter your email address"
+            className="w-[300px] sm:w-[400px] mx-auto py-3 px-6 text-VeryDarkBlue rounded-md border outline-none md:mx-0"
+            onChange={handleChange}
+            value={email}
+          />
+          <img
+            src="./images/icon-error.svg"
+            alt="error-icon"
+            className="absolute right-5 top-5"
+          />
+          <p className="absolute left-[15px] bottom-[10px] text-xs italic">
+            Whoops, make sure it's an email
+          </p>
+        </div>
+        <button
+          type="submit"
+          className="w-[300px] bg-SoftRed sm:w-[400px] mx-auto px-6 py-3 rounded-md border border-SoftRed shadow-md hover:bg-white hover:border-SoftRed hover:text-SoftRed transition-all duration-300 md:m-0 md:ml-6 md:w-[150px]"
+        >
+          Contact Us
+        </button>
+      </form>
     </section>
   );
-}
+};
 
 export default Contact;
