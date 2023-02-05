429d1a307fcb6d6fb570ea3aaef352643492b6c5
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-04 18:24:31 -0800

    add useeffect to remove warning or success after 3 seconds

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Contact = () => {
   const [email, setEmail] = useState("");
-  const [isError,setIsError] = useState(false);
-  const [isCorrect,setIsCorrect] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const validateEmail = (email) => {
     return String(email)
@@ -19,10 +19,27 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(validateEmail(email)) {
-      
+    if (validateEmail(email)) {
+      setIsCorrect(true);
+      setIsError(false);
+    } else {
+      setIsError(true);
+      setIsCorrect(false);
     }
-  }
+
+    setEmail("");
+  };
+
+  useEffect(() => {
+    let timeOut = setTimeout(()=> {
+      setIsCorrect(false);
+      setIsError(false)
+    },3000)
+
+    return () => {
+      clearTimeout(timeOut)
+    }
+  },[isCorrect,isError])
 
   return (
     <section className="bg-SoftBlue px-8 py-12 text-white text-center">
@@ -32,8 +49,15 @@ const Contact = () => {
       <h3 className="max-w-[500px] mx-auto mt-4 mb-6 text-2xl sm:text-3xl md:text-4xl lg:max-w-[600px] lg:text-5xl lg:mb-12">
         Stay up-to-date with what we’re doing
       </h3>
-      <form className="flex flex-col items-center md:flex-row md:items-start md:justify-center">
-        <div className="relative p-1 pb-8 mb-5 bg-SoftRed rounded-md">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center md:flex-row md:items-start md:justify-center"
+      >
+        <div
+          className={`relative p-1 pb-8 mb-5 rounded-md ${
+            isError && "bg-SoftRed"
+          } ${isCorrect && "bg-[#57faa9]"}`}
+        >
           <input
             type="email"
             placeholder="Enter your email address"
@@ -41,14 +65,23 @@ const Contact = () => {
             onChange={handleChange}
             value={email}
           />
-          <img
-            src="./images/icon-error.svg"
-            alt="error-icon"
-            className="absolute right-5 top-5"
-          />
-          <p className="absolute left-[15px] bottom-[10px] text-xs italic">
-            Whoops, make sure it's an email
-          </p>
+          {isError && (
+            <>
+              <img
+                src="./images/icon-error.svg"
+                alt="error-icon"
+                className="absolute right-5 top-5"
+              />
+              <p className="absolute left-[15px] bottom-[10px] text-xs italic">
+                Whoops, make sure it's an email
+              </p>
+            </>
+          )}
+          {isCorrect && (
+            <p className="absolute left-[15px] bottom-[10px] text-xs italic">
+              Your email has been sent, thank you !
+            </p>
+          )}
         </div>
         <button
           type="submit"
