34f5d7edff8525bd52fa8993b550902d22e1d7f9
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-04 19:43:15 -0800

    add hover on footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,21 @@ const Footer = () => {
         className="img-filter w-48 mb-12"
       />
       <div className="flex flex-col items-center text-white">
-        <a href="#" className="mb-10 uppercase tracking-wide text-base">
+        <a href="#" className="mb-10 uppercase tracking-wide hover:text-SoftRed transition-colors duration-300">
           Features
         </a>
-        <a href="#" className="mb-10 uppercase tracking-wide text-base">
+        <a href="#" className="mb-10 uppercase tracking-wide hover:text-SoftRed transition-colors duration-300">
           Pricing
         </a>
-        <a href="#" className="mb-10 uppercase tracking-wide text-base">
+        <a href="#" className="mb-12 uppercase tracking-wide hover:text-SoftRed transition-colors duration-300">
           Contact
         </a>
       </div>
-      <div className="flex items-center space-x-8">
-        <a href="#" className="text-white hover:text-SoftRed">
+      <div className="flex items-center space-x-12">
+        <a href="#" className="text-white hover:text-SoftRed transition-colors duration-300">
           <FaFacebook size={40} />
         </a>
-        <a href="#" className="text-white hover:text-SoftRed">
+        <a href="#" className="text-white hover:text-SoftRed transition-colors duration-300">
           <FaTwitter size={40}/>
         </a>
       </div>
